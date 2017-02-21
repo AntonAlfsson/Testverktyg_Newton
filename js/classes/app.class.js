4f9b6216092a492a74960de5300b282929a42267
AntonAlfsson <Anton.Alfsson@gmail.com>
2017-02-21 09:53:16 +0100

    Implementerat det nya skeleton

---
+++ js/classes/app.class.js
@@ -0,0 +1,19 @@
+class App {
+
+  constructor(){
+
+    // Development tool
+    new BootstrapSize().display('body');
+      
+    new navbar().display('body');
+      
+      
+      
+       new Fragor().display('body');
+      
+      
+    new footer().display('body');
+    
+
+  }
+}
