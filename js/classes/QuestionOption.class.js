ddfde16911c450e8beebf23389f8ed5b24a0d7d1
Anna Fock <anna@fock.se>
2017-02-22 11:18:38 +0100

    Döpte om klassen Option till QuestionOption

--- js/classes/QuestionOption.class.js
+++ js/classes/QuestionOption.class.js
@@ -2,8 +2,20 @@ class Option extends Base {
     
     constructor(){
         super();
-        this.getAllByOptions();
+        this.getAllByOption();
     }
+
+    getAllByOption(){ // metod för att hämta Title från tabellen Question
+          
+          this.db.all({  
+          },(data)=>{
+            console.log("nu går jag in i metoden getAllByOption")
+            $('#option1').html(data[0]);  
+            console.log(data[1].options);
+              
+              
+              //$('.question-head').html("Ny text");            
+          });
 
     static get sqlQueries(){
 
