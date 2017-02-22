efc14b98c678d9574a9cc5f237a02897516b99ca
Richard-Gustafsson <rille117@hotmail.com>
2017-02-22 12:58:26 +0100

    Återställde teacherprofile.js

--- js/classes/teacherprofile.class.js
+++ js/classes/teacherprofile.class.js
@@ -2,41 +2,7 @@ class teacherprofile extends Base {
 
     constructor(propertyValues){
         super(propertyValues);
-        //this.getTName();
+
     }
 
-    //Detta är ett test att sätta en namn
-    // Kallar in metoden som sätter namn
-    getTName(){
-
-      this.db.tName({
-      },(data)=>{
-        $('.teacherName').empty();
-        $('.teacherName').html("test");
-        for(var i = 0; i < data.length; i++){
-
-          
-
-          $('.teacherName').html('test');
-
-
-
-        }
-      });
-      }
-
-
-
-
-      static get sqlQueries(){
-
-        return {
-          tName: `
-            select Name from Person
-          `,
-        allStudents:`
-        select pNr, Name, roll, klass from Person
-    `
-        }
-      }
-    }
+}
