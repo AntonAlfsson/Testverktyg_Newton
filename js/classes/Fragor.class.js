0d52d91ff3896dfc6d4830b815f6195b44647624
Anna Fock <anna@fock.se>
2017-03-01 17:02:38 +0100

    Jobbade vidare på att skapa questionoption template

--- js/classes/Fragor.class.js
+++ js/classes/Fragor.class.js
@@ -3,7 +3,6 @@ class Fragor extends Base {
 	constructor(propertyValues, callback){
 		super(propertyValues);
 
-		//this.idTest = propertyValues.idTest;
 		console.log(propertyValues.idTest);
 
 		var test = new Test(propertyValues.idTest, ()=> {
@@ -17,11 +16,6 @@ class Fragor extends Base {
 		
 
 	}
-
-
-
-
-
 }
 
 
